1138a1921902e4bd184e58d122c4f9f81b894b18
amirrasooli69 <amirrasooli69@gmail.com>
2022-04-23 20:14:45 +0430

    fix search

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,9 +5,11 @@ import { getCoin } from '../services/api';
 
 //Components
 import Loader from './Loader';
+import Coin from './Coin';
 const Landing = () => {
 
     const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         const fetchAPI = async () => {
@@ -20,14 +22,28 @@ const Landing = () => {
 
     },[])
 
+    const searchHandler = event => {
+        setSearch(event.target.value);
+    }
+
+    const searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
+
     return (
         <>
 
-            <input type="text" placeholder='Enter Name for Search' />
+            <input type="text" placeholder='Enter Name for Search' value={search} onChange={searchHandler}/>
             {
                 coins.length ? 
                     
-                        coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+                        searchedCoins.map(coin => <Coin 
+                            key={coin.id}
+                            image={coin.image}
+                            name={coin.name}
+                            symbol={coin.symbol}
+                            price={coin.current_price}
+                            marketCap={coin.market_cap}
+                            priceChange={coin.price_change_percentage_24h}
+                        />)
                     
                 :
                     <Loader />
